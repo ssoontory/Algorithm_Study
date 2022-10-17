19b23cc9a002cf239579fc84f0fbf01a39598cd4
leees00n <leees0053@gmail.com>
2022-10-17 14:33:03 +0900

    algorithm

---
+++ src/JavaStudy/UseMath/guestNum.java
@@ -0,0 +1,26 @@
+package JavaStudy.UseMath;
+
+import java.util.Scanner;
+
+public class guestNum {
+    public static void main(String[] args) {
+        int input = 0;
+        int answer = 0;
+
+        answer = (int)(Math.random() * 100) + 1;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.print("1과 100사이의 정수를 입력하세요.");
+            input = sc.nextInt();
+
+            if (input > answer) {
+                System.out.println("더 작은 수로 다시 시도해보세요");
+            } else if (input < answer) {
+                    System.out.println("더 큰 수로 다시 시도해보세요.");
+            }
+        }while (input != answer);
+        System.out.println("정답입니다!!");
+
+    }
+}
