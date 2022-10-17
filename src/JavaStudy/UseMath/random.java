7757176c0f06a46d9cfeb2d6a3ed6ee6c062159a
leees00n <leees0053@gmail.com>
2022-10-17 10:00:08 +0900

    algorithm

---
+++ src/JavaStudy/UseMath/random.java
@@ -0,0 +1,14 @@
+package JavaStudy.UseMath;
+
+public class random {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 20; i++) {
+         //원하는 개수만큼 곱해주기   System.out.println(Math.random()*10);
+            System.out.println((int)(Math.random()*10+1));
+        }
+    }
+
+
+}
